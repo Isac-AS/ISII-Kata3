90d8e2b9aa3181f72d91190edbe911ce00be3f40
Isac <isac.as0001@gmail.com>
2021-10-20 22:28:35 +0100

    Kata3 version 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,9 +1,19 @@
 package kata3;
 
+import java.util.Random;
+
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay();
+        Histogram<String> histogram = new Histogram();
+        Random rand = new Random();
+        for (int i = 0; i < rand.nextInt(50) + 10; i++) {
+            if (rand.nextBoolean()) histogram.increment("gmail.com");
+            if (rand.nextBoolean()) histogram.increment("ulpgc.es");
+            if (rand.nextBoolean()) histogram.increment("ull.es");
+            if (rand.nextBoolean()) histogram.increment("hotmail.com");
+        }
+        HistogramDisplay histo = new HistogramDisplay(histogram);
         histo.execute();
     }
     
